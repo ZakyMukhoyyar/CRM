345c5a00509be52d3a6e00d8fce6f64267f5bc33
bayuhendra <bayuhendra1078@gmail.com>
2017-09-13 21:40:32 +0700

    add function broadcast email sukses register

--- com.agit.crm.main/src/main/java/com/agit/crm/main/viewmodel/register/RegisterVM.java
+++ com.agit.crm.main/src/main/java/com/agit/crm/main/viewmodel/register/RegisterVM.java
@@ -48,7 +48,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.zkoss.bind.BindContext;
 import org.zkoss.bind.annotation.BindingParam;
@@ -418,6 +425,47 @@ public class RegisterVM {
             userService.delete(userDTO.getUserName());
             CommonViewModel.showInformationMessagebox("User Name " + userDTO.getUserName() + " has successfully deleted", UserNavigation.USER_SEARCH, null, window);
         }
+
+        final String username = "bayuhendra1078@gmail.com";
+        final String passwordEmail = "bayuhendra1993";
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, passwordEmail);
+            }
+        });
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("bayuhendra1078@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(userDTO.getUserSpecificationDTO().getEmail()));
+
+            message.setSubject("Registrasi CRM");
+            message.setText("Dear  " + userDTO.getUserName()
+                    + "\n\n Selamat, Anda sudah berhasil mendaftar App Candidate Recruitment di PT. Astra Graphia Information Technology"
+                    + "\n "
+                    + "\n Nama Lengkap  : " + userDTO.getUserSpecificationDTO().getFullName()
+                    + "\n Email         : " + userDTO.getUserSpecificationDTO().getEmail()
+                    + "\n No KTP        : " + userDTO.getUserSpecificationDTO().getKtp()
+                    + "\n\n Mohon simpan email ini sebagai referensi atas registrasi CRM anda.. "
+                    + "\n\n Terimakasih. "
+                    + "\n\n PT. Astra Graphia Information Technology. "
+            );
+            message.setSentDate(new Date());
+
+            Transport.send(message);
+
+            System.out.println("Sending Email Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     protected String getLatestObjectID(ListModelList list, String attribute) {
