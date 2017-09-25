8d71b0775fc8f316dc73328348a874a92c8069eb
bayuhendra <bayuhendra1078@gmail.com>
2017-09-25 09:56:24 +0700

    update profile mahasiswa

--- com.agit.crm.main/src/main/java/com/agit/crm/main/viewmodel/WorkspaceVM.java
+++ com.agit.crm.main/src/main/java/com/agit/crm/main/viewmodel/WorkspaceVM.java
@@ -1,13 +1,27 @@
 package com.agit.crm.main.viewmodel;
 
+import com.agit.crm.common.application.CivitasService;
+import com.agit.crm.common.application.DomisiliService;
+import com.agit.crm.common.application.JurusanService;
+import com.agit.crm.common.application.KetrampilanService;
+import com.agit.crm.common.application.MinatService;
+import com.agit.crm.common.dto.crm.CivitasDTO;
+import com.agit.crm.common.dto.crm.DomisiliDTO;
+import com.agit.crm.common.dto.crm.JurusanDTO;
+import com.agit.crm.common.dto.crm.KetrampilanDTO;
+import com.agit.crm.common.dto.crm.MinatDTO;
 import com.agit.crm.common.dto.usermanagement.PrivilegeDTO;
 import com.agit.crm.common.dto.usermanagement.UserDTO;
 import com.agit.crm.common.layout.PrivilegeTreeModel;
 import com.agit.crm.common.security.SecurityUtil;
+import com.agit.crm.shared.type.JenisKelaminType;
+import com.agit.crm.shared.type.PendidikanType;
+import com.agit.crm.shared.type.TingkatanType;
 import com.agit.crm.shared.zul.CommonViewModel;
 import static com.agit.crm.shared.zul.CommonViewModel.showQuestionMessagebox;
 import com.agit.crm.user.management.application.PrivilegeService;
 import com.agit.crm.user.management.application.RoleService;
+import com.agit.crm.user.management.application.UserService;
 import com.agit.crm.user.management.application.security.SecurityCacheHelper;
 import com.agit.crm.util.DateUtil;
 import java.util.ArrayList;
@@ -23,6 +37,7 @@ import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.GlobalCommand;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
@@ -33,6 +48,7 @@ import org.zkoss.zk.ui.event.SerializableEventListener;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Tab;
 import org.zkoss.zul.Tabbox;
@@ -53,6 +69,24 @@ public class WorkspaceVM {
     @WireVariable
     RoleService roleService;
 
+    @WireVariable
+    UserService userService;
+
+    @WireVariable
+    DomisiliService domisiliService;
+
+    @WireVariable
+    MinatService minatService;
+
+    @WireVariable
+    KetrampilanService ketrampilanService;
+
+    @WireVariable
+    JurusanService jurusanService;
+
+    @WireVariable
+    CivitasService civitasService;
+
     private UserDTO user;
     private PrivilegeTreeModel model;
     private String rightSelectedTab;
@@ -60,6 +94,36 @@ public class WorkspaceVM {
     private String role;
     private String uriPhoto;
     private String sourceDashboard;
+
+    private UserDTO userDTO = new UserDTO();
+    private List<UserDTO> userDTOs = new ArrayList<>();
+    private List<UserDTO> userDTO2s = new ArrayList<>();
+    private List<DomisiliDTO> domisilis = new ArrayList<DomisiliDTO>();
+    private List<MinatDTO> minats = new ArrayList<MinatDTO>();
+    private List<String> listMinat = new ArrayList<>();
+    private List<String> listKetrampilan = new ArrayList<>();
+    private List<String> listDomisili = new ArrayList<>();
+    private List<String> listCivitas = new ArrayList<>();
+    private List<String> listJurusan = new ArrayList<>();
+    private ListModelList<JenisKelaminType> jenisKelaminTypes;
+    private ListModelList<PendidikanType> pendidikanTypes;
+    private ListModelList<TingkatanType> tingkatanTypes1;
+    private ListModelList<TingkatanType> tingkatanTypes2;
+    private ListModelList<TingkatanType> tingkatanTypes3;
+    private ListModelList<TingkatanType> tingkatanTypes4;
+    private ListModelList<TingkatanType> tingkatanTypes5;
+    private List<KetrampilanDTO> ketrampilans = new ArrayList<KetrampilanDTO>();
+    private List<JurusanDTO> jurusans = new ArrayList<JurusanDTO>();
+    private List<CivitasDTO> civitass = new ArrayList<CivitasDTO>();
+
+    private String fullName;
+    private String ktp;
+    private String domisiliSelect;
+    private String minatSelect;
+    private String jurusanSelect;
+    private String ketrampilanSelect;
+    private String civitasSelect;
+    private String userID;
 
     @Wire
     private Tabs tabs;
@@ -153,19 +217,17 @@ public class WorkspaceVM {
                 Component comp = Executions.createComponents(selected.getUrl(), null, null);
                 tabPanel.appendChild(comp);
             }
+        } else if (model.isObjectOpened(selected)) {
+            model.removeOpenObject(selected);
         } else {
-            if (model.isObjectOpened(selected)) {
-                model.removeOpenObject(selected);
-            } else {
-                model.clearOpen();
-                model.addOpenPath(model.getPath(selected));
-                int[] path = model.getPath(selected);
-                int[] openPath = new int[]{};
-                for (int i = 0; i < path.length; i++) {
-                    openPath = Arrays.copyOf(openPath, openPath.length + 1);
-                    openPath[openPath.length - 1] = path[i];
-                    model.addOpenPath(openPath);
-                }
+            model.clearOpen();
+            model.addOpenPath(model.getPath(selected));
+            int[] path = model.getPath(selected);
+            int[] openPath = new int[]{};
+            for (int i = 0; i < path.length; i++) {
+                openPath = Arrays.copyOf(openPath, openPath.length + 1);
+                openPath[openPath.length - 1] = path[i];
+                model.addOpenPath(openPath);
             }
         }
     }
@@ -255,6 +317,14 @@ public class WorkspaceVM {
         }
     }
 
+    @Command("buttonKlikProfile")
+    @NotifyChange("userDTO")
+    public void buttonDetailProfil(@BindingParam("object") UserDTO obj, @ContextParam(ContextType.VIEW) Window window) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userDTO", obj);
+        CommonViewModel.navigateToWithoutDetach("/crm/mahasiswa/beranda_profile_mahasiswa.zul", window, params);
+    }
+
     @Command("buttonLogout")
     public void buttonLogout(@ContextParam(ContextType.VIEW) Window window) {
         CommonViewModel.navigateTo("login.zul", window, null);
@@ -323,6 +393,254 @@ public class WorkspaceVM {
 
     public void setSourceDashboard(String sourceDashboard) {
         this.sourceDashboard = sourceDashboard;
+    }
+
+    public UserDTO getUserDTO() {
+        return userDTO;
+    }
+
+    public void setUserDTO(UserDTO userDTO) {
+        this.userDTO = userDTO;
+    }
+
+    public List<UserDTO> getUserDTOs() {
+        return userDTOs;
+    }
+
+    public void setUserDTOs(List<UserDTO> userDTOs) {
+        this.userDTOs = userDTOs;
+    }
+
+    public List<UserDTO> getUserDTO2s() {
+        return userDTO2s;
+    }
+
+    public void setUserDTO2s(List<UserDTO> userDTO2s) {
+        this.userDTO2s = userDTO2s;
+    }
+
+    public List<DomisiliDTO> getDomisilis() {
+        return domisilis;
+    }
+
+    public void setDomisilis(List<DomisiliDTO> domisilis) {
+        this.domisilis = domisilis;
+    }
+
+    public List<MinatDTO> getMinats() {
+        return minats;
+    }
+
+    public void setMinats(List<MinatDTO> minats) {
+        this.minats = minats;
+    }
+
+    public List<String> getListMinat() {
+        return listMinat;
+    }
+
+    public void setListMinat(List<String> listMinat) {
+        this.listMinat = listMinat;
+    }
+
+    public List<String> getListKetrampilan() {
+        return listKetrampilan;
+    }
+
+    public void setListKetrampilan(List<String> listKetrampilan) {
+        this.listKetrampilan = listKetrampilan;
+    }
+
+    public List<String> getListDomisili() {
+        return listDomisili;
+    }
+
+    public void setListDomisili(List<String> listDomisili) {
+        this.listDomisili = listDomisili;
+    }
+
+    public List<String> getListCivitas() {
+        return listCivitas;
+    }
+
+    public void setListCivitas(List<String> listCivitas) {
+        this.listCivitas = listCivitas;
+    }
+
+    public List<String> getListJurusan() {
+        return listJurusan;
+    }
+
+    public void setListJurusan(List<String> listJurusan) {
+        this.listJurusan = listJurusan;
+    }
+
+    public ListModelList<JenisKelaminType> getJenisKelaminTypes() {
+        return new ListModelList<>(JenisKelaminType.values());
+    }
+
+    public void setJenisKelaminTypes(ListModelList<JenisKelaminType> jenisKelaminTypes) {
+        this.jenisKelaminTypes = jenisKelaminTypes;
+    }
+
+    public ListModelList<PendidikanType> getPendidikanTypes() {
+        return new ListModelList<>(PendidikanType.values());
+    }
+
+    public void setPendidikanTypes(ListModelList<PendidikanType> pendidikanTypes) {
+        this.pendidikanTypes = pendidikanTypes;
+    }
+
+    public ListModelList<TingkatanType> getTingkatanTypes1() {
+        return new ListModelList<>(TingkatanType.values());
+    }
+
+    public void setTingkatanTypes1(ListModelList<TingkatanType> tingkatanTypes1) {
+        this.tingkatanTypes1 = tingkatanTypes1;
+    }
+
+    public ListModelList<TingkatanType> getTingkatanTypes2() {
+        return new ListModelList<>(TingkatanType.values());
+    }
+
+    public void setTingkatanTypes2(ListModelList<TingkatanType> tingkatanTypes2) {
+        this.tingkatanTypes2 = tingkatanTypes2;
+    }
+
+    public ListModelList<TingkatanType> getTingkatanTypes3() {
+        return new ListModelList<>(TingkatanType.values());
+    }
+
+    public void setTingkatanTypes3(ListModelList<TingkatanType> tingkatanTypes3) {
+        this.tingkatanTypes3 = tingkatanTypes3;
+    }
+
+    public ListModelList<TingkatanType> getTingkatanTypes4() {
+        return new ListModelList<>(TingkatanType.values());
+    }
+
+    public void setTingkatanTypes4(ListModelList<TingkatanType> tingkatanTypes4) {
+        this.tingkatanTypes4 = tingkatanTypes4;
+    }
+
+    public ListModelList<TingkatanType> getTingkatanTypes5() {
+        return new ListModelList<>(TingkatanType.values());
+    }
+
+    public void setTingkatanTypes5(ListModelList<TingkatanType> tingkatanTypes5) {
+        this.tingkatanTypes5 = tingkatanTypes5;
+    }
+
+    public List<KetrampilanDTO> getKetrampilans() {
+        return ketrampilans;
+    }
+
+    public void setKetrampilans(List<KetrampilanDTO> ketrampilans) {
+        this.ketrampilans = ketrampilans;
+    }
+
+    public List<JurusanDTO> getJurusans() {
+        return jurusans;
+    }
+
+    public void setJurusans(List<JurusanDTO> jurusans) {
+        this.jurusans = jurusans;
+    }
+
+    public List<CivitasDTO> getCivitass() {
+        return civitass;
+    }
+
+    public void setCivitass(List<CivitasDTO> civitass) {
+        this.civitass = civitass;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getKtp() {
+        return ktp;
+    }
+
+    public void setKtp(String ktp) {
+        this.ktp = ktp;
+    }
+
+    public String getDomisiliSelect() {
+        return domisiliSelect;
+    }
+
+    public void setDomisiliSelect(String domisiliSelect) {
+        this.domisiliSelect = domisiliSelect;
+    }
+
+    public String getMinatSelect() {
+        return minatSelect;
+    }
+
+    public void setMinatSelect(String minatSelect) {
+        this.minatSelect = minatSelect;
+    }
+
+    public String getJurusanSelect() {
+        return jurusanSelect;
+    }
+
+    public void setJurusanSelect(String jurusanSelect) {
+        this.jurusanSelect = jurusanSelect;
+    }
+
+    public String getKetrampilanSelect() {
+        return ketrampilanSelect;
+    }
+
+    public void setKetrampilanSelect(String ketrampilanSelect) {
+        this.ketrampilanSelect = ketrampilanSelect;
+    }
+
+    public String getCivitasSelect() {
+        return civitasSelect;
+    }
+
+    public void setCivitasSelect(String civitasSelect) {
+        this.civitasSelect = civitasSelect;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Tabs getTabs() {
+        return tabs;
+    }
+
+    public void setTabs(Tabs tabs) {
+        this.tabs = tabs;
+    }
+
+    public Tabpanels getTabPanels() {
+        return tabPanels;
+    }
+
+    public void setTabPanels(Tabpanels tabPanels) {
+        this.tabPanels = tabPanels;
+    }
+
+    public Tabbox getTabbox() {
+        return tabbox;
+    }
+
+    public void setTabbox(Tabbox tabbox) {
+        this.tabbox = tabbox;
     }
 
 }
---
+++ com.agit.crm.user.management/src/main/resources/script/init_script_25092017 .sql
@@ -0,0 +1,69 @@
+-- ----------------------------
+-- Records of mst_privilege
+-- ----------------------------
+INSERT INTO "public"."mst_privilege" VALUES ('0', 'ROOT', 'ROOT', 'DELETED', null, 't', 'Dashboard', 'Dashboard', null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('1', 'ADMINISTRATOR', 'Administrator', 'ACTIVE', 'ROOT', 't', 'Pengurus', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('2', 'GROUP', 'Group', 'ACTIVE', 'ADMINISTRATOR', 't', 'Pengelolaan Grup', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('3', 'GROUP_SEARCH', 'Group Search', 'ACTIVE', 'GROUP', 't', 'Pemeliharaan Grup', 'Pemeliharaan Grup', null, '~./ui/fnd/group/search.fnd.group.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('4', 'GROUP_UPDATE', 'Group Update', 'ACTIVE', 'GROUP_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('5', 'GROUP_VIEW', 'Group View', 'ACTIVE', 'GROUP_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('6', 'GROUP_DELETE', 'Group Delete', 'ACTIVE', 'GROUP_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('7', 'GROUP_CREATE', 'Group Create', 'ACTIVE', 'GROUP', 't', 'Pembuatan Grup', 'Pembuatan Grup', null, '~./ui/fnd/group/create.fnd.group.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('8', 'USER_MANAGEMENT', 'User', 'ACTIVE', 'ADMINISTRATOR', 't', 'Pengelolaan Pengguna', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('9', 'USER_SEARCH', 'User Search', 'ACTIVE', 'USER_MANAGEMENT', 't', 'Pemeliharaan Pengguna', 'Pemeliharaan Pengguna', null, '~./ui/fnd/user/search.fnd.user.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('10', 'USER_UPDATE', 'User Update', 'ACTIVE', 'USER_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('11', 'USER_VIEW', 'User View', 'ACTIVE', 'USER_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('12', 'USER_DELETE', 'User Delete', 'ACTIVE', 'USER_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('13', 'USER_RESET_PASSWORD', 'User Reset Password', 'ACTIVE', 'USER_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('14', 'USER_LOCK_UNLOCK', 'User Lock Unlock', 'ACTIVE', 'USER_SEARCH', 'f', null, null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('15', 'USER_CREATE', 'User Create', 'ACTIVE', 'USER_MANAGEMENT', 't', 'Pembuatan Pengguna', 'Pembuatan Pengguna', null, '~./ui/fnd/user/create.fnd.user.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('16', 'USER_RELEASE', 'User Release', 'ACTIVE', 'USER_MANAGEMENT', 't', 'Penghapusan Pengguna', 'Penghapusan Pengguna', null, '~./ui/fnd/user/release.fnd.user.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('17', 'MENU', 'Menu Dashboard', 'ACTIVE', 'ADMINISTRATOR', 't', 'Pengelolaan Menu', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('18', 'MENU_MANAGEMENT', 'Menu Dashboard', 'ACTIVE', 'MENU', 't', 'Beranda - Menu', 'Beranda - Menu', null, '~./ui/fnd/menu/dashboard.privilege.zul', '2016-01-01 00:00:00', 'SYSTEM');
+
+INSERT INTO "public"."mst_privilege" VALUES ('19', 'ADMIN_MANAGEMENT', 'Admin', 'ACTIVE', 'ADMINISTRATOR', 't', 'Pengelolaan Admin', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('20', 'EVENT_MANAGEMENT', 'Event Management', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Acara', 'Beranda - Acara', null, '/crm/admin/event/dashboard_event.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('21', 'FORUM_MANAGEMENT', 'Forum Management', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Forum', 'Beranda - Forum', null, '/crm/admin/forum/dashboard_forum.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('22', 'KETRAMPILAN_MANAGEMENT', 'Ketrampilan Management', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data History Ketrampilan', 'Beranda - Data History Ketrampilan', null, '/crm/admin/ketrampilan/dashboard_ketrampilan.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('23', 'MINAT_MANAGEMENT', 'Minat Management', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data History Minat', 'Beranda - Data History Minat', null, '/crm/admin/minat/dashboard_minat.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('24', 'LOWONGAN_MANAGEMENT', 'Lowongan Management', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Lowongan', 'Beranda - Lowongan', null, '/crm/admin/lowongan/dashboard_lowongan.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('25', 'JURUSAN_MANAGEMENT', 'Jurusan Management', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data History Jurusan', 'Beranda - Data History Jurusan', null, '/crm/admin/jurusan/dashboard_jurusan.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('26', 'DATA_MAHASISWA', 'Dashboard Data Mahasiswa', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Mahasiswa', 'Beranda - Mahasiswa', null, '/crm/admin/dataRegister/dashboard_data_register_mahasiswa.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('27', 'DATA_APPLY_LOWONGAN', 'Dashboard Data Apply Lowongan', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data Apply Lowongan', 'Beranda - Data Apply Lowongan', null, '/crm/admin/dataApplyLowongan/dashboard_pelamar.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('28', 'DATA_APPLY_EVENT', 'Dashboard Data Apply Acara', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data Apply Acara', 'Beranda - Data Apply Acara', null, '/crm/admin/dataApplyAcara/dashboard_apply_acara.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('29', 'DATA_CIVITAS', 'Dashboard Data Civitas', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data History Civitas', 'Beranda - Data History Civitas', null, '/crm/admin/civitas/dashboard_civitas.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('30', 'DATA_DOMISILI', 'Dashboard Data Domisili', 'ACTIVE', 'ADMIN_MANAGEMENT', 't', 'Beranda - Data History Domisili', 'Beranda - Data History Domisili', null, '/crm/admin/domisili/dashboard_domisili.zul', '2016-01-01 00:00:00', 'SYSTEM');
+
+INSERT INTO "public"."mst_privilege" VALUES ('31', 'MAHASISWA_MANAGEMENT', 'Admin', 'ACTIVE', 'ADMINISTRATOR', 't', 'Pengelolaan Data Mahasiswa', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('32', 'LOWONGAN', 'Beranda Lowongan', 'ACTIVE', 'MAHASISWA_MANAGEMENT', 't', 'Informasi - Lowongan', 'Informasi - Lowongan', null, '/crm/mahasiswa/beranda_lowongan_mahasiswa.zul', '2016-01-01 00:00:00', 'SYSTEM');
+-- INSERT INTO "public"."mst_privilege" VALUES ('33', 'PROFILE', 'Update Profile', 'ACTIVE', 'MAHASISWA_MANAGEMENT', 't', 'Profil - Profilku', 'Profil - Profilku', null, '/crm/mahasiswa/beranda_profile_mahasiswa.zul', '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('34', 'FORUM', 'Beranda Mahasiswa Forum', 'ACTIVE', 'MAHASISWA_MANAGEMENT', 't', 'Informasi - Forum', 'Informasi - Forum', null, '/crm/mahasiswa/tab_forum.zul', '2016-01-01 00:00:00', 'SYSTEM');
+
+INSERT INTO "public"."mst_privilege" VALUES ('35', 'RECRUITER_MANAGEMENT', 'Admin', 'ACTIVE', 'ADMINISTRATOR', 't', 'Pengelolaan Data Pelamar', null, null, null, '2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_privilege" VALUES ('36', 'DATA_PELAMAR', 'Data Pelamar', 'ACTIVE', 'RECRUITER_MANAGEMENT', 't', 'Data - Pelamar', 'Data - Pelamar', null, '/crm/recruiter/DashboardRecruiter.zul', '2016-01-01 00:00:00', 'SYSTEM');
+
+-- ----------------------------
+-- Records of mst_role
+-- ----------------------------
+INSERT INTO "public"."mst_role" VALUES ('1', 'SUPERADMIN', 'SUPERADMIN', 'SUPERADMIN', 'ACTIVE', '2016-01-01 00:00:00', 'SUPERADMIN');
+INSERT INTO "public"."mst_role" VALUES ('2', 'ADMIN', 'ADMIN', 'ADMIN', 'ACTIVE', '2016-01-01 00:00:00', 'SUPERADMIN');
+INSERT INTO "public"."mst_role" VALUES ('3', 'MAHASISWA', 'MAHASISWA', 'MAHASISWA', 'ACTIVE', '2016-01-01 00:00:00', 'SUPERADMIN');
+INSERT INTO "public"."mst_role" VALUES ('4', 'RECUITER', 'RECUITER', 'RECUITER', 'ACTIVE', '2016-01-01 00:00:00', 'SUPERADMIN');
+
+-- ----------------------------
+-- Records of mst_role_privilege
+-- ----------------------------
+INSERT INTO "public"."mst_role_privilege" VALUES ('1', '1', 'ALLOW', '0');
+INSERT INTO "public"."mst_role_privilege" VALUES ('2', '19', 'ALLOW', '0');
+INSERT INTO "public"."mst_role_privilege" VALUES ('2', '31', 'ALLOW', '1');
+INSERT INTO "public"."mst_role_privilege" VALUES ('3', '31', 'ALLOW', '0');
+INSERT INTO "public"."mst_role_privilege" VALUES ('4', '36', 'ALLOW', '0');
+
+-- ----------------------------
+-- Records of mst_user
+-- ----------------------------
+INSERT INTO "public"."mst_user" VALUES ('1', 'SUPERADMIN', '$2a$10$vW7zns/PRqfHgaxN3DpJuuH2n1mI152gMxO6Lnm4X6zrAEXDPBAUC', 'USER001', '1200101', 'ACTIVE', '1', 'Super Admin', 'superadmin@agit.com', '0811111111', 'SYSTEM', '001','321354616', null, 'TI', 'JAKARTA', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', null, null, '0', '2016-09-21 15:37:06.305', null, null, '0:0:0:0:0:0:0:1', '0:0:0:0:0:0:0:1', '1e2fuhqu103ts1uv76m4k3czc', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,'2016-01-01 00:00:00', 'SYSTEM');
+INSERT INTO "public"."mst_user" VALUES ('2', 'ADMIN', '$2a$10$57J57UK4Wxa6GUWig2QyA.KYv5GgiHeXUIUppRtmczWSlTJd35iba', 'USER002', '1200102', 'ACTIVE', '2', 'Admin', 'admin@agit.com', '0811111111', 'SUPERADMIN', '001', '321354617', null, 'TI', 'BANDUNG', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', null, null, '0', null, null, null, '0:0:0:0:0:0:0:1', '0:0:0:0:0:0:0:1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '2016-01-01 00:00:00', 'SUPERADMIN');
+INSERT INTO "public"."mst_user" VALUES ('3', 'MAHASISWA', '$2a$10$57J57UK4Wxa6GUWig2QyA.KYv5GgiHeXUIUppRtmczWSlTJd35iba', 'USER003', '1200102', 'ACTIVE', '3', 'Admin', 'admin@agit.com', '0811111111', 'SUPERADMIN', '001','321354618', null, 'TI', 'BANDUNG', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', null, null, '0', null, null, null, '0:0:0:0:0:0:0:1', '0:0:0:0:0:0:0:1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '2016-01-01 00:00:00', 'SUPERADMIN');
+INSERT INTO "public"."mst_user" VALUES ('4', 'RECRUITER', '$2a$10$57J57UK4Wxa6GUWig2QyA.KYv5GgiHeXUIUppRtmczWSlTJd35iba', 'USER004', '1200102', 'ACTIVE', '4', 'Admin', 'admin@agit.com', '0811111111', 'SUPERADMIN', '001','321354618', null, 'TI', 'BANDUNG', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', '1970-01-01 00:00:00', '1970-01-01 23:59:00', null, null, '0', null, null, null, '0:0:0:0:0:0:0:1', '0:0:0:0:0:0:0:1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '2016-01-01 00:00:00', 'SUPERADMIN');
+
