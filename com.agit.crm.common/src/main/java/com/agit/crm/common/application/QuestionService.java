2719f0fc090ec09cae0066270d098d40046f2de4
bayuhendra <bayuhendra1078@gmail.com>
2018-07-10 13:52:14 +0700

    add service question

--- com.agit.crm.common/src/main/java/com/agit/crm/common/application/QuestionService.java
+++ com.agit.crm.common/src/main/java/com/agit/crm/common/application/QuestionService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.agit.crm.common.application;
 
 import com.agit.crm.common.dto.customer.feedback.QuestionDTO;
